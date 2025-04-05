84a7966c74e7a77186ba2475213e993140e1a846
변기덕 <gideok@byeongideog-ui-MacBookPro.local>
2025-04-05 19:34:12 +0900

    practice-done

--- 백준/1-2.py
+++
@@ -1,3 +0,0 @@
-a = int(input("A를 입력하시오:"))
-b = int(input("B를 입력하시오:"))
-print(f"the sum of {a} +{b} = {a + b}")
--- 백준/1.py
+++
@@ -1,3 +0,0 @@
-num = int(input("A를 입력하시오:"))
-C = int(input("B를 입력하시오:"))
-print(num + C)
